96a1890d4bbb82c0d7762568ea59fe1c0607c240
Alec Tiefenthal <alectief@gmail.com>
2016-01-05 14:07:19 -0500

    Added detection of some generic types
    For method return types, and field types.

--- src/problem/asm/ClassFieldVisitor.java
+++ src/problem/asm/ClassFieldVisitor.java
@@ -2,10 +2,11 @@ package problem.asm;
 
 import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.FieldVisitor;
+import org.objectweb.asm.Type;
 
 public class ClassFieldVisitor extends ClassVisitor {
 
-	UMLGraph graph;
+	private UMLGraph graph;
 	public ClassFieldVisitor(int arg0) {
 		super(arg0);
 		// TODO Auto-generated constructor stub
@@ -30,7 +31,8 @@ public class ClassFieldVisitor extends ClassVisitor {
 
 	public FieldVisitor visitField(int access, String name, String desc, String signature, Object value)
 	{
-		this.graph.addField(new UMLField(name, access, desc));
+		//System.out.println(Type.getType(signature).getElementType());
+		this.graph.addField(new UMLField(name, access, desc, signature));
 		FieldVisitor toDecorate = super.visitField(access, name, desc, signature, value);
 		//String type = Type.getType(desc).getClassName();
 		//System.out.println("    "+type+" "+name);
@@ -38,5 +40,5 @@ public class ClassFieldVisitor extends ClassVisitor {
 		//this.graph.addField(new UMLField(name, access, type));
 		return toDecorate;
 	}
-
+	
 }
