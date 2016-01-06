d09f96e0912843c772b1f6f68a06b650eef6ac96
mcgarrdg <mcgarrdg@rose-hulman.edu>
2016-01-05 19:57:47 -0500

    Test cases for UMLClass added

--- test/milestone1/UMLClassTest.java
+++ test/milestone1/UMLClassTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 import java.util.ArrayList;
 
 import org.junit.Test;
+import org.objectweb.asm.Opcodes;
 
 import problem.asm.UMLClass;
 import problem.asm.UMLField;
@@ -17,14 +18,29 @@ public class UMLClassTest {
 	private String extension;
 	private ArrayList<UMLMethod> methods = new ArrayList<UMLMethod>();
 	private ArrayList<UMLField> fields = new ArrayList<UMLField>();
-	private ArrayList<String> implementations= new ArrayList<String>();
+	private String[] implementations= new String[1];
 	
 	public UMLClassTest() {
 		this.name = "MyClass";
 		this.extension = "MyDadsClass"; 
-//		methods.add(new UMLMethod("coolMethod", "+", ));
+		ArrayList<String> argumentTypes = new ArrayList<String>(); 
+		argumentTypes.add("int");
+		argumentTypes.add("String");
+		String mname = "multiplyString";
+		String returnType = "String"; 
+		String returnGenericType = null;
+		int accessType = Opcodes.ACC_PUBLIC;
 		
+		clazz = new UMLClass(this.name, this.extension, this.implementations);
+		UMLMethod m = new UMLMethod(mname, accessType, argumentTypes, returnGenericType, returnType);
+		methods.add(m);
+		clazz.addMethod(m);
 
+		String fname = "bestField";		
+		UMLField f = new UMLField(fname, returnType, returnGenericType, accessType);
+		fields.add(f);
+		clazz.addField(f);
+		
 	}
 	
 	@Test
@@ -49,13 +65,20 @@ public class UMLClassTest {
 	@Test
 	public void testFields() {
 		ArrayList<UMLField> testfields = clazz.getFields();
-		assertTrue(testfields.size() == this.methods.size());
+		assertTrue(testfields.size() == this.fields.size());
 		for(int i = 0; i < testfields.size(); i++) {
-			assertEquals(testfields.get(i), this.methods.get(i));
+			assertEquals(testfields.get(i), this.fields.get(i));
 		}
 	}
 	
-	
+	@Test
+	public void testImplementations() {
+		ArrayList<String> testimps = clazz.getImplementations();
+		assertTrue(testimps.size() == this.implementations.length);
+		for(int i = 0; i < testimps.size(); i++) {
+			assertEquals(testimps.get(i), this.implementations[i]);
+		}
+	}
 	
 	
 	
