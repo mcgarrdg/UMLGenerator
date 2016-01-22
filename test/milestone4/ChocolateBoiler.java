946dd3764dbab0ec88656fb0fe28de6362ebb32d
mcgarrdg <mcgarrdg@rose-hulman.edu>
2016-01-21 23:16:38 -0500

    added the Chocolate Boiler code

---
+++ test/milestone4/ChocolateBoiler.java
@@ -0,0 +1,46 @@
+package milestone4;
+
+public class ChocolateBoiler {
+	private boolean empty;
+	private boolean boiled;
+
+	private static ChocolateBoiler uniqueInstance;
+
+	private ChocolateBoiler() {
+		empty = true;
+		boiled = false;
+	}
+	public static ChocolateBoiler getInstance() {
+		if (uniqueInstance == null) {
+			uniqueInstance = new ChocolateBoiler(); 
+		}
+		return uniqueInstance;
+	}
+	
+	public void fill() {
+		if (isEmpty()) {
+			empty = false;
+			boiled = false;
+		}
+	}
+	
+	public void drain() {
+		if (!isEmpty() && isBoiled()) {
+			boiled = true;
+		}
+	}
+	
+	public void boil() {
+		if (!isEmpty() && !isBoiled()) {
+			boiled = true;
+		}
+	}
+	
+	public boolean isEmpty() {
+		return empty;
+	}
+	
+	public boolean isBoiled() {
+		return boiled;
+	}
+}
